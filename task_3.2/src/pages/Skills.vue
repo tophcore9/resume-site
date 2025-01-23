9e9eb3de78956057004b414e38b09f7589dcb610
Oleksii Paziura <o.paziura@imcagro.com.ua>
2025-01-23 14:32:30 +0200

    Add skills page

--- task_3.2/src/pages/Skills.vue
+++ task_3.2/src/pages/Skills.vue
@@ -1,17 +1,39 @@
 <template>
     <div class="page-content">
         <h1 class="page-title__h1">Skills</h1>
+        <h2 class="page-title__h2">Programming languages & tools</h2>
+        <div class="skills-icons">
+            <img src="../assets/icons/html5.svg" alt="html5" />
+            <img src="../assets/icons/css3.svg" alt="css3" />
+            <img src="../assets/icons/js.svg" alt="js" />
+            <img src="../assets/icons/jquery.svg" alt="jquery" />
+            <img src="../assets/icons/sass.svg" alt="sass" />
+            <img src="../assets/icons/less.svg" alt="less" />
+            <img src="../assets/icons/bootstrap.svg" alt="bootstrap" />
+            <img src="../assets/icons/bulma.svg" alt="bulma" />
+            <img src="../assets/icons/gulp.svg" alt="gulp" />
+            <img src="../assets/icons/npm.svg" alt="npm" />
+            <img src="../assets/icons/vue.svg" alt="vue" />
+            <img src="../assets/icons/nuxt.svg" alt="nuxt" />
+            <img src="../assets/icons/webpack.svg" alt="webpack" />
+            <img src="../assets/icons/vite.svg" alt="vite" />
+        </div>
+        <h2 class="page-title__h2">Workflow</h2>
+        <ul class="list">
+            <li class="list-item">Mobile-First, Responsive Design</li>
+            <li class="list-item">Cross Browser Testing & Debugging</li>
+            <li class="list-item">Cross Functional Teams</li>
+            <li class="list-item">Agile Development & Scrum</li>
+        </ul>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-export default defineComponent({
-    
-});
+export default defineComponent({});
 </script>
 
 <style scoped>
-    
+    @import url(../assets/css/skills.css);
 </style>
